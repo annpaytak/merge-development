685840f6b1c87292b9fea90620bad53ca752d787
annpaytak <anna.paytak@gmail.com>
2023-01-18 11:06:56 +0200

    change purge to content in tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
     extend: {
       fontFamily: {
